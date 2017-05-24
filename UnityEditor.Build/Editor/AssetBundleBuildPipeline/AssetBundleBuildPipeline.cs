9eed04eec5382b7f5403b55c36acc6fa59082e9b
Stephen Palmer <stephenp@unity3d.com>
2017-05-24 15:38:44 +0200

    split build pipeline into two steps - generate commandset and write commandset

--- UnityEditor.Build/Editor/AssetBundleBuildPipeline/AssetBundleBuildPipeline.cs
+++ UnityEditor.Build/Editor/AssetBundleBuildPipeline/AssetBundleBuildPipeline.cs
@@ -20,26 +20,25 @@ namespace UnityEditor.Build.AssetBundle
             return settings;
         }
 
-        public static bool BuildAssetBundles(BuildSettings settings, BuildInput input, out BuildOutput output)
+        public static bool GenerateCommandSet(BuildSettings settings, BuildInput input, out BuildCommandSet output)
         {
             var buildTimer = new Stopwatch();
             buildTimer.Start();
 
-            output = new BuildOutput();
-            var compression = BuildCompression.DefaultUncompressed;
+            output = new BuildCommandSet();
 
-            // Rebuild sprite atlas cache for correct dependency calculation & writing
-            Packer.RebuildAtlasCacheIfNeeded(settings.target, true, Packer.Execution.Normal);
-
-            var commandList = new List<BuildCommandSet.Command>();
-
-            var prepareScene = new PrepareScene();
-            var scenePacker = new ScenePacker();
- 
             var bundleOutputFolder = settings.outputFolder;
             settings.outputFolder = Path.Combine(settings.outputFolder, "_resources");
             Directory.CreateDirectory(settings.outputFolder);
 
+            // Rebuild sprite atlas cache for correct dependency calculation & writing
+            Packer.RebuildAtlasCacheIfNeeded(settings.target, true, Packer.Execution.Normal);
+            
+            var commandList = new List<BuildCommandSet.Command>();
+            
+            var prepareScene = new PrepareScene();
+            var scenePacker = new ScenePacker();
+            
             // Generate command set for scenes
             for(var x = 0; x < input.definitions.Length; x++)
             {
@@ -55,48 +54,58 @@ namespace UnityEditor.Build.AssetBundle
                             UnityEngine.Debug.LogError("Scene build of " + assetPath + " failed in PrepareScene");
                             continue;
                         }
-
+                        
                         BuildCommandSet sceneCommands;
                         if(!scenePacker.Convert(sceneInfo, out sceneCommands, false))
                         {
                             UnityEngine.Debug.LogError("Scene build of " + assetPath + " failed in ScenePacker");
                             continue;
                         }
-
+                        
                         // Fix up asset bundle name
                         sceneCommands.commands[0].assetBundleName = def.assetBundleName;
-
+                        
                         commandList.AddRange(sceneCommands.commands);
                         ArrayUtility.RemoveAt<BuildInput.Definition>(ref input.definitions, x--);
                     }
                 }
             }
-
+            
             // Generate command set for loose bundles
             BuildCommandSet assetCommands;
             var packer = new Unity5Packer();
             if (!packer.Convert(input, settings.target, out assetCommands))
                 return false;
-
+            
             // Combine scene and loose bundle commands
             commandList.AddRange(assetCommands.commands);
-
+            
             var allCommands = new BuildCommandSet();
             allCommands.commands = commandList.ToArray();
-
+            
             // Calculate assetBundleDependencies
-            BuildCommandSet depCommands;
             var dependencyCalculator = new Unity5DependencyCalculator();
-            if (!dependencyCalculator.Convert(allCommands, out depCommands))
+            if (!dependencyCalculator.Convert(allCommands, out output))
                 return false;
 
+            return true;
+        }
+
+        public static bool ExecuteCommandSet(BuildSettings settings, BuildCommandSet commands, out BuildOutput output)
+        {
+            output = new BuildOutput();
+            var compression = BuildCompression.DefaultUncompressed;
+
+            var bundleOutputFolder = settings.outputFolder;
+            settings.outputFolder = Path.Combine(settings.outputFolder, "_resources");
+            Directory.CreateDirectory(settings.outputFolder);
             //DebugPrintCommandSet(ref depCommands);
 
             // TODO: implement incremental building when LLAPI supports it
 
             // Write out resource files
             var resourceWriter = new ResourceWriter();
-            if (!resourceWriter.Convert(depCommands, settings, out output))
+            if (!resourceWriter.Convert(commands, settings, out output))
                 return false;
 
             // Archive and compress resource files
@@ -108,7 +117,7 @@ namespace UnityEditor.Build.AssetBundle
             // Generate Unity5 compatible manifest files
             string[] manifestfiles;
             var manifestWriter = new Unity5ManifestWriter();
-            if (!manifestWriter.Convert(depCommands, output, crc, bundleOutputFolder, out manifestfiles))
+            if (!manifestWriter.Convert(commands, output, crc, bundleOutputFolder, out manifestfiles))
                 return false;
 
             return true;
